cd9c703cdb68ded61a41c820f53cb2e669685664
emilydoswell <70842968+emilydoswell@users.noreply.github.com>
2022-12-07 11:37:00 +0000

    Station Database

---
+++ Station.sql
@@ -0,0 +1,12 @@
+use classpractise;
+
+create table Station(
+stationId int primary key,
+stationName varchar(20) not null);
+
+insert into Station
+values(1, "Station1"),
+(2, "Station2"),
+(3, "Station3"),
+(4, "Station4"),
+(5, "Station5");
