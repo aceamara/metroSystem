5929b4fb6f4545225585fbd793e67202da377fbb
emilydoswell <70842968+emilydoswell@users.noreply.github.com>
2022-12-12 13:49:03 +0000

    updates

--- Both APIs and Client/MetroCustomer/src/main/java/com/maria/service/CustomerServiceImpl.java
+++ Both APIs and Client/MetroCustomer/src/main/java/com/maria/service/CustomerServiceImpl.java
@@ -24,7 +24,7 @@ public class CustomerServiceImpl implements CustomerService{
 
 	
 	public Customer getCustomer(int id){
-		 return dao.findById(id).get();
+		 return dao.findById(id).orElse(null);
 	
 	}
 	
